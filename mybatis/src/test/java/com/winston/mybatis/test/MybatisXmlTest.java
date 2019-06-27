c2a076c45d1966a440bc681eb2a2776d5585acd1
winston.wang <940945444@qq.com>
2019-06-27 10:16:56 +0800

    新增mybatis加载properties文件

--- mybatis/src/test/java/com/winston/mybatis/test/MybatisXmlTest.java
+++ mybatis/src/test/java/com/winston/mybatis/test/MybatisXmlTest.java
@@ -5,10 +5,14 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author Winston.Wang
@@ -31,5 +35,26 @@ public class MybatisXmlTest {
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
         List<User> list = sqlSession.selectList("com.winston.mapper.UserMapper.selectAllUser");
         list.stream().forEach(System.out::println);
+    }
+
+    /**
+     * 加載properties文件
+     * @author  Winston.Wang
+     * @date 2019/6/26
+     **/
+    @Test
+    public  void testProperties(){
+
+        ClassLoader loader = MybatisXmlTest.class.getClassLoader();
+        InputStream resourceAsStream = loader.getResourceAsStream("jdbc.properties");
+
+        Properties properties = new Properties();
+        try {
+            properties.load(resourceAsStream);
+            String driver = properties.getProperty("driver");
+            Assert.assertEquals(driver,"com.mysql.jdbc.Driver");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
